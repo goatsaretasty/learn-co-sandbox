8a25f13513abef270e988703e8bee5b93c23c57a
Ade Adesina <janet.adesina@gmail.com>
2021-02-22 15:36:18 +0000

    Automatically backed up by Learn

---
+++ functions.js
@@ -0,0 +1,37 @@
+//function doNothing() {} is known as a noop 
+
+// function sayHello(){
+//   console.log('hello!')
+// }
+
+// sayHello();
+
+// function sayHelloToIsabel() {
+//   console.log('hello Isabel!')
+// }
+
+// sayHelloToIsabel();
+
+// function sayHelloTo(firstName) {
+//   console.log(`hey there, ${firstName}!`)
+// }
+
+// sayHelloTo('Adesola');
+// sayHelloTo('Saj');
+// sayHelloTo('Maximillion');
+
+// function say(greeting, firstName) {
+//   return `${greeting}, ${firstName}!`
+//   console.log('I was called!')
+// }
+
+// function say(greeting, firstName) {
+//   console.log('I was called!')
+//   return `${greeting}, ${firstName}!`
+// }
+
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`
+  console.log('I was called!')
+}
+say("Howdy", "partner");
